5686af0a80928756ae7f11b1ce854559fd25753e
abotw <>
2025-07-18 11:11:59 +0800

    [+] Update clean_home_temp.sh

--- clean_home_temp.sh
+++ clean_home_temp.sh
@@ -4,7 +4,7 @@ TARGET="$HOME/__TEMP"
 
 rm -rf "${TARGET:?}/"*
 
-# 加载任务: launchctl load ~/Library/LaunchAgents/com.user.cleartemp.plist
+# 加载任务: launchctl load ~/Library/Mobile\ Documents/com~apple~CloudDocs/AppData/db_LaunchAgents/com.mql.cleartemp.plist
 # 修改任务:
-# 1. launchctl unload ~/Library/LaunchAgents/com.user.cleartemp.plist
-# 2. launchctl load ~/Library/LaunchAgents/com.user.cleartemp.plist
+# 1. launchctl unload ~/Library/Mobile\ Documents/com~apple~CloudDocs/AppData/db_LaunchAgents/com.mql.cleartemp.plist
+# 2. launchctl load ~/Library/Mobile\ Documents/com~apple~CloudDocs/AppData/db_LaunchAgents/com.mql.cleartemp.plist
